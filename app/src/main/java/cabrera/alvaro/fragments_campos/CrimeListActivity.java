b92b0faaba169ec20e2d6e7368e9dc12e98dd590
Christian <dam2a06@SINF2003SRVR2.LOCAL>
2019-02-08 20:38:33 +0100

    Initial commit

---
+++ app/src/main/java/cabrera/alvaro/fragments_campos/CrimeListActivity.java
@@ -0,0 +1,11 @@
+package cabrera.alvaro.fragments_campos;
+
+import android.support.v4.app.Fragment;
+
+public class CrimeListActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+}
